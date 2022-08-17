fb6dc4213512d33f31446a53fcf40d4276703ba6
Sergio Lopez <sergio@softnya.com>
2022-08-17 09:15:21 -0500

    Inicialización de modelo

---
+++ backend/app/model/countries.model.js
@@ -0,0 +1,22 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      name: String,
+      code: String,
+      capital: String,
+      region: String,
+      currency: Array,
+      language: Array,
+      flag: String,
+      demonym: String
+    },
+    { timestamps: true }
+  );
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+  const Countries = mongoose.model("countries", schema);
+  return Countries;
+};
