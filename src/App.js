d10ac4c65ef4909b94d76550c159552e68bed286
Sergio Lopez <sergio@softnya.com>
2022-08-16 21:44:30 -0500

    Poner logo en header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
 import './assets/scss/App.scss';
 import 'uikit/dist/css/uikit.min.css';
 import 'uikit/dist/css/uikit-core.min.css';
@@ -6,8 +6,21 @@ import 'uikit/dist/js/uikit.min.js';
 
 function App() {
   return (
-    <div className="uk-container CountriesApp">
+    <div className="CountriesApp">
+      <div className="uk-section uk-section-xsmall uk-section-primary" id="uk-header">
+        <div className="uk-flex uk-flex-center uk-flex-middle">
+          <img src={logo} className="uk-logo" alt="My Countries" />
+          <h3 className="uk-margin-remove-top uk-margin-remove-bottom uk-margin-small-left">My Countries</h3>
+        </div>
+      </div>
+      <div className="uk-section" id="uk-content">
+        <div className="uk-container">
 
+        </div>
+      </div>
+      <div className="uk-section" id="uk-footer">
+
+      </div>
     </div>
   );
 }
